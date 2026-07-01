8e05e0071646429cd3a87a9e737eb2a44f761783
Person885 <keyboaqwerty@gmail.com>
2026-07-01 10:31:52 -0600

    Adjust maxAll parameters for complexity calculations

--- js/complexity/complexity-max-all.js
+++ js/complexity/complexity-max-all.js
@@ -8,11 +8,11 @@ let ComplexityMaxAll = {
     }
     generalHighestSweep(() => ComplexityGenerators.highest(), types);
     generalMaxAll(types.filter(x => x <= 9).map(x => ComplexityGenerator(x)), ComplexityPoints);
-    generalMaxAll(types.filter(x => x > 9 && x <= 12).map(x => PowerUpgrade(x - 8)), ComplexityPoints);
-    generalMaxAll(types.filter(x => x > 12).map(x => PowerShardUpgrade(x - 11)), PowerShards);
+    generalMaxAll(types.filter(x => x > 8 && x <= 11).map(x => PowerUpgrade(x - 7)), ComplexityPoints);
+    generalMaxAll(types.filter(x => x > 11).map(x => PowerShardUpgrade(x - 11)), PowerShards);
   },
   maxUpgrades() {
-    this.maxAll([10, 11, 12]);
+    this.maxAll([9, 10, 11]);
   },
   maxGenerators() {
     this.maxAll([1, 2, 3, 4, 5, 6, 7, 8, 9]);
