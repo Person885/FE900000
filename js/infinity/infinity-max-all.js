3a3fce0ec10177b3a288eac742a58d0226b0a788
Person885 <keyboaqwerty@gmail.com>
2026-07-01 10:25:24 -0600

    Adjust maxAll function parameters and calls

--- js/infinity/infinity-max-all.js
+++ js/infinity/infinity-max-all.js
@@ -10,16 +10,16 @@ let InfinityMaxAll = {
   },
   maxAll(types) {
     if (types === undefined) {
-      types = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+      types = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     }
     generalHighestSweep(() => InfinityGenerators.highest(), types);
-    generalMaxAll(types.filter(x => x > 9).map(x => InfinityUpgrade(x - 9)), InfinityPoints);
+    generalMaxAll(types.filter(x => x > 8).map(x => InfinityUpgrade(x - 8)), InfinityPoints);
     generalMaxAll(types.filter(x => x <= 9).map(x => InfinityGenerator(x)), InfinityPoints);
   },
   maxUpgrades() {
-    this.maxAll([10, 11]);
+    this.maxAll([9, 10]);
   },
   maxGenerators() {
-    this.maxAll([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    this.maxAll([1, 2, 3, 4, 5, 6, 7, 8]);
   }
 }
